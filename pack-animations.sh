49bddc47ce5f2a9188e41c6a76c88a6206db3f44
bmaupin <bmaupincode@gmail.com>
2015-12-12 08:36:48 -0500

    Abort if any logo files are > 4 MiB

--- pack-animations.sh
+++ pack-animations.sh
@@ -5,4 +5,18 @@ prefix=bootanimation-
 # Cleanup
 rm -f $prefix*.zip
 
+# Check for logo files > 4 MiB (The logo partition on Moto G devices is exactly 4 MiB)
+error=false
+for logo in `ls -1 boot-animations/*/logo/*/logo.bin`; do
+    if [ `stat --printf="%s" $logo` -gt 4194304 ]; then
+        echo "ERROR: logo file is greater than 4 MiB: $logo"
+        error=true
+    fi
+done
+
+if [ "$error" = true ]; then
+    echo "Please reduce the size of logo files and try again"
+    exit 1
+fi
+
 for animation in `ls -1 boot-animations`; do cd boot-animations/$animation; zip -r ../../$prefix$animation.zip .; cd ../..; zip -r $prefix$animation.zip META-INF; done
