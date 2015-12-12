0d496ea0e24be7fc2bfbc830ab8a178d5915a6f0
bmaupin <bmaupin@users.noreply.github.com>
2015-12-12 08:25:24 -0500

    Don't output an error if the zip files haven't been created yet

--- pack-animations.sh
+++ pack-animations.sh
@@ -3,6 +3,6 @@
 PREFIX=bootanimation-
 
 # Cleanup
-rm $PREFIX*.zip
+rm -f $PREFIX*.zip
 
 for ANIMATION in `ls -1 boot-animations`; do cd boot-animations/$ANIMATION; zip -r ../../$PREFIX$ANIMATION.zip .; cd ../..; zip -r $PREFIX$ANIMATION.zip META-INF; done
